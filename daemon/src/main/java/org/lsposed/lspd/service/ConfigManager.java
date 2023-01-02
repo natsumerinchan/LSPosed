906d6568a73266c821b34c6ba4de776a36d405e9
LoveSy <shana@zju.edu.cn>
2023-01-03 00:58:20 +0800

    Add more module service interfaces

--- daemon/src/main/java/org/lsposed/lspd/service/ConfigManager.java
+++ daemon/src/main/java/org/lsposed/lspd/service/ConfigManager.java
@@ -232,6 +232,7 @@ public class ConfigManager {
                 } catch (PackageParser.PackageParserException e) {
                     Log.w(TAG, "failed to parse parse " + module.apkPath, e);
                 }
+                module.service = new LSPInjectedModuleService(module);
                 modules.add(module);
             }
         }
@@ -1006,9 +1007,9 @@ public class ConfigManager {
     }
 
     // this is slow, avoid using it
-    public String getModule(int uid) {
+    public Module getModule(int uid) {
         for (var module : cachedModule.values()) {
-            if (module.appId == uid % PER_USER_RANGE) return module.packageName;
+            if (module.appId == uid % PER_USER_RANGE) return module;
         }
         return null;
     }
